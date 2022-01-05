7502e8fb34f7aad153b6af8315606e38fc5ba89b
Julie Nisbet <jmnisbet@gmail.com>
2022-01-05 11:39:23 -0800

    adding edit page

---
+++ src/components/dogs/DogForm.js
@@ -0,0 +1,81 @@
+import React from 'react';
+
+export default function DogForm({
+  name,
+  setName,
+  bio,
+  setBio,
+  image,
+  setImage,
+  age,
+  setAge,
+  breed,
+  setBreed,
+  handleSubmit,
+}) {
+  return (
+    <div className="dog-form">
+      <form>
+        <h2>Enter Dog Details</h2>
+
+        <div className="form-control">
+          <label>Name:</label>
+          <input
+            placeholder="Name"
+            type="text"
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
+        </div>
+        <div className="form-control">
+          <label>Bio:</label>
+          <textarea
+            placeholder="Bio"
+            value={bio}
+            onChange={(e) => {
+              setBio(e.target.value);
+            }}
+          />
+        </div>
+        <div className="form-control">
+          <label>Age: </label>
+          <input
+            placeholder="Age"
+            type="number"
+            value={age}
+            onChange={(e) => {
+              setAge(e.target.value);
+            }}
+          />
+        </div>
+        <div className="form-control">
+          <label>Image: </label>
+          <input
+            placeholder="Image URL"
+            type="text"
+            value={image}
+            onChange={(e) => {
+              setImage(e.target.value);
+            }}
+          />
+        </div>
+        <div className="form-control">
+          <label>Breed: </label>
+          <input
+            placeholder="Breed"
+            type="text"
+            value={breed}
+            onChange={(e) => {
+              setBreed(e.target.value);
+            }}
+          />
+        </div>
+        <div className="action">
+          <button onClick={handleSubmit}>Save</button>
+        </div>
+      </form>
+    </div>
+  );
+}
--- src/services/dogs.js
+++ src/services/dogs.js
@@ -5,6 +5,19 @@ export async function fetchDogs() {
 }
 
 export async function fetchDogById(id) {
-  const resp = await client.from('dogs').select('*').eq('id', id);
+  const resp = await client.from('dogs').select('*').eq('id', id).single();
+  return checkError(resp);
+}
+
+export async function updateDog(id, name, bio, image, breed) {
+  const resp = await client
+    .from('dogs')
+    .update({
+      name,
+      bio,
+      image,
+      breed,
+    })
+    .eq('id', id);
   return checkError(resp);
 }
